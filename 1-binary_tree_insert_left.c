c8e493b8d7eadf352cdf201d660ec5460dd083ea
mouhcine wildane <muhssenwildane@gmail.com>
2024-01-29 22:18:29 +0100

    1

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t Node;
+	
+	Node = malloc(sizeof(binary_tree_t));
+	if ((Node == NULL) or (parent == NULL))
+		return NULL;
+	Node->left = NULL;
+	Node->right = NULL;
+	Node->parent = parent;
+	Node->n = value;
+	if parent->left != NULL
+	{
+		Node->left = parent->left;
+	}
+	parent->left = Node;
+	return Node;
+}
