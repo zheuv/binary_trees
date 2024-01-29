7b9de5d94cb5698139a268050602809fb6e183e9
mouhcine wildane <muhssenwildane@gmail.com>
2024-01-29 23:24:28 +0100

    third t

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,20 @@
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+        binary_tree_t *Node;
+
+        Node = malloc(sizeof(binary_tree_t));
+        if ((Node == NULL) || (parent == NULL))
+                return NULL;
+        Node->left = NULL;
+        Node->right = NULL;
+        Node->parent = parent;
+        Node->n = value;
+        if (parent->right != NULL)
+        {
+                Node->right = parent->right;
+                parent->right->parent = Node;
+        }
+        parent->right = Node;
+        return Node;
+}
+
---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,16 @@
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+
+	if (tree->left != NULL)
+		binary_tree_delete(tree->left);
+	if (tree->right != NULL)
+		binary_tree_delete(tree->right);
+
+	tree->parent = NULL;
+	tree->left = NULL;
+	tree->right = NULL;
+	tree->n = NULL;
+	free(tree);
+}
