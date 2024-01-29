0eac96fcac0d653ea5fb1ed8badf0e6ce214785a
mouhcine wildane <muhssenwildane@gmail.com>
2024-01-29 22:32:01 +0100

    task 1 corr

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -16,6 +16,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if parent->left != NULL
 	{
 		Node->left = parent->left;
+		parent->left->parent = Node;
 	}
 	parent->left = Node;
 	return Node;
