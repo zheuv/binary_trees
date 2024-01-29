6a8e0e05679e7a9f9201cf2d58146ef5ea41d5a7
mouhcine wildane <muhssenwildane@gmail.com>
2024-01-29 22:40:51 +0100

    task 1 corr

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -7,13 +7,13 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *Node;
 	
 	Node = malloc(sizeof(binary_tree_t));
-	if ((Node == NULL) or (parent == NULL))
+	if ((Node == NULL) || (parent == NULL))
 		return NULL;
 	Node->left = NULL;
 	Node->right = NULL;
 	Node->parent = parent;
 	Node->n = value;
-	if parent->left != NULL
+	if (parent->left != NULL)
 	{
 		Node->left = parent->left;
 		parent->left->parent = Node;
