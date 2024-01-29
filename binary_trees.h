3a703c34abc07323f33d5d8df9d50257fda08c69
Mouhcine Wildane <117749214+zheuv@users.noreply.github.com>
2024-01-29 20:55:50 +0100

    Update binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -15,11 +15,8 @@ struct binary_tree_s
     struct binary_tree_s *parent;
     struct binary_tree_s *left;
     struct binary_tree_s *right;
-};
-typedef struct binary_tree_s bst_t;
-typedef struct binary_tree_s avl_t;
-typedef struct binary_tree_s binary_tree_t;
-typedef struct binary_tree_s heap_t;
+} binary_tree_t;
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 #endif
