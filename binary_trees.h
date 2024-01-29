3d252b5c5ff4f9f3137b6a0585ac6f23b85afad4
Mouhcine Wildane <117749214+zheuv@users.noreply.github.com>
2024-01-29 21:14:52 +0100

    Update binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -5,18 +5,20 @@
 #include <stdlib.h>
 #include <string.h>
 
-static int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
-static size_t _height(const binary_tree_t *tree);
-void binary_tree_print(const binary_tree_t *tree);
-
 struct binary_tree_s
 {
     int n;
     struct binary_tree_s *parent;
     struct binary_tree_s *left;
     struct binary_tree_s *right;
-} binary_tree_t;
+};
+typedef struct binary_tree_s binary_tree_t;
+
+static int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
+static size_t _height(const binary_tree_t *tree);
+void binary_tree_print(const binary_tree_t *tree);
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 #endif
+
