0df6dcb77ae55c09c0b37315211d1eedc0b65ff0
Mouhcine Wildane <117749214+zheuv@users.noreply.github.com>
2024-01-30 16:57:13 +0100

    Update 3-binary_tree_delete.c

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,5 +1,21 @@
 #include "binary_trees.h"
-
+/**
+ * binary_tree_delete - Deletes an entire binary tree rooted at a given node.
+ *
+ * @tree: Pointer to the root node of the binary tree to be deleted.
+ *
+ * This function recursively deletes all nodes in the binary tree starting from
+ * the given root node. It sets the parent, left child, right child, and the value
+ * of each node to NULL or 0 before freeing the memory allocated for the node.
+ * The root node itself is also freed.
+ *
+ * @tree should be set to NULL after calling this function to avoid dangling pointers.
+ *
+ * Note: If the nodes of the binary tree contain dynamically allocated memory,
+ * it's the responsibility of the user to free that memory before calling this function.
+ *
+ * @tree: Pointer to the root node of the binary tree to be deleted.
+ */
 void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree == NULL)
