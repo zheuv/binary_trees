e8af6f3c888cfc334a1677d35266a7921a5d9c3b
Mouhcine Wildane <117749214+zheuv@users.noreply.github.com>
2024-01-30 20:17:36 +0100

    Update 7-binary_tree_inorder.c

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -18,9 +18,7 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (!tree || !func)
 		return;
-	if (tree->left)
-		binary_tree_inorder(tree->left, func);
+	binary_tree_inorder(tree->left, func);
 	func(tree);
-	if (tree->right)
-		binary_tree_inorder(tree->right, func);
+	binary_tree_inorder(tree->right, func);
 }
