8712a4c9431f5cfd4e31f024f10f2013300fcac1
mouhcine wildane <muhssenwildane@gmail.com>
2024-01-31 17:35:28 +0100

    11 corr

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -4,7 +4,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (!tree)
 		return 0;
-	static size_t size = 1;
+	size_t size = 1;
 	if (tree->left)
 		size += binary_tree_size(tree->left);
 	if (tree->right)
