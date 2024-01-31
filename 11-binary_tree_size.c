9979844eb381bbd59697305d17b0d6869a3e876f
Mouhcine Wildane <117749214+zheuv@users.noreply.github.com>
2024-01-31 17:37:28 +0100

    Update 11-binary_tree_size.c

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
-
+/**
+ * binary_tree_size -  creates a binary tree node.
+ *
+ * @tree: pointer to the parent node of the node to create.
+ *
+ * Return: pointer to the new node, or NULL on failure
+ */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (!tree)
