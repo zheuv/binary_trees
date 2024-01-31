b34d28b0849fcf8f43633ba2d44bda65e7c054b1
mouhcine wildane <muhssenwildane@gmail.com>
2024-01-31 18:58:29 +0100

    descri

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,22 +1,54 @@
 #include "binary_trees.h"
-
-int binary_tree_balance2(const binary_tree_t *tree, const binary_tree_t *tree1)
+/**
+ * binary_tree_balance2 - Calculate the balance factor of a binary tree node
+ *                        with respect to another given node.
+ *
+ * @tree: Pointer to the binary tree node to calculate the balance factor for
+ * @hypothetical_root: Pointer to the binary tree node with respect to which the balance
+ *         factor is calculated.
+ *
+ * This function calculates the balance factor of a binary tree node by
+ * comparing the heights of its left and right subtrees with respect to
+ * another given node. The balance factor is determined by subtracting the
+ * height of the right subtree from the height of the left subtree. The
+ * calculation is performed recursively.
+ *
+ * @tree: Pointer to the binary tree node to calculate the balance factor for
+ * @hypothetical_root: Pointer to the binary tree node with respect to which the balance
+ *         factor is calculated.
+ *
+ * Return: The balance factor of the binary tree node.
+ */
+int binary_tree_balance2(const binary_tree_t *tree, const binary_tree_t *hypothetical_root)
 {
-	size_t right = 1;
-	size_t left = 1;
+	int right = 1;
+	int left = 1;
 
 	if (!tree)
 		return (0);
-	left += binary_tree_balance2(tree->left, tree1);
-	right += binary_tree_balance2(tree->right, tree1);
+	left += binary_tree_balance2(tree->left, hypothetical_root);
+	right += binary_tree_balance2(tree->right, hypothetical_root);
 
-	if (tree == tree1)
+	if (tree == hypothetical_root)
 		return (left - right);
 	if (left > right)
 		return (left);
 	return (right);
 }
-
+/**
+ * binary_tree_balance - Calculate the balance factor of a binary tree node.
+ *
+ * @tree: Pointer to the binary tree node to calculate the balance factor for.
+ *
+ * This function calculates the balance factor of a binary tree node by calling
+ * the binary_tree_balance2 function with the same node as both arguments. The
+ * balance factor is a measure of the height difference between the left and
+ * right subtrees of the given node.
+ *
+ * @tree: Pointer to the binary tree node to calculate the balance factor for.
+ *
+ * Return: The balance factor of the binary tree node.
+ */
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	return (binary_tree_balance2(tree, tree));
