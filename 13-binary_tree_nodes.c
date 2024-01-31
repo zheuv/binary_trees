758e390035cbae06850bab498efa1ddf6a4c4608
Mouhcine Wildane <117749214+zheuv@users.noreply.github.com>
2024-01-31 18:17:48 +0100

    Update 13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,5 +1,10 @@
 #include "binary_trees.h"
-
+/**
+ * binary_tree_nodes - Counts nodes with at least 1 child in a binary tree.
+ *
+ * @tree: Pointer to the root node of the tree to count nodes.
+ * Return: Number of nodes with at least 1 child.
+ */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	size_t Nodes = 0;
