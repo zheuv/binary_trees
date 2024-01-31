b6ec8843edffa46651a962466db284711a76b1e5
Mouhcine Wildane <117749214+zheuv@users.noreply.github.com>
2024-01-31 17:38:58 +0100

    Update 11-binary_tree_size.c

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -9,8 +9,9 @@
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (!tree)
-		return 0;
+		return (0);
 	size_t size = 1;
+
 	if (tree->left)
 		size += binary_tree_size(tree->left);
 	if (tree->right)
