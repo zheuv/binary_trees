6e7d132bdd0e1e1ab77a4e5b9b2865e045744635
Mouhcine Wildane <117749214+zheuv@users.noreply.github.com>
2024-02-02 09:48:13 +0100

    Update 10-binary_tree_depth.c

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -12,8 +12,8 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 	size_t depth = 0;
 
 	if (!tree)
-		return 0;
+		return (0);
 	if (tree->parent)
 		depth = 1 + binary_tree_depth(tree->parent);
-	return depth;
+	return (depth);
 }
